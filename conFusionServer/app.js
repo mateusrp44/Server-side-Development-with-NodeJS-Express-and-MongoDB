29f367dc3338fb9b597b62e357bb5eca32e917a9
mateusrp44 <mateusrpinto44@gmail.com>
2019-06-03 14:42:31 -0300

    Assignment 2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -65,12 +65,13 @@ app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
-
 
 module.exports = app;
