005278d11e6ff494fff0307fdc8c11552fd5d4d3
mateusrp44 <mateusrpinto44@gmail.com>
2019-06-13 14:49:15 -0300

    Mongoose Population

--- conFusionServer/models/user.js
+++ conFusionServer/models/user.js
@@ -3,19 +3,18 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
+    firstname: {
+      type: String,
+        default: ''
+    },
+    lastname: {
+      type: String,
+        default: ''
+    },
     admin:   {
         type: Boolean,
         default: false
-    }/*,
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:  {
-        type: String,
-        required: true
-    }*/
+    }
 });
 
 User.plugin(passportLocalMongoose);
